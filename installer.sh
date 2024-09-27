c485b7ff794acadeeacc08c78917e215a9f162c9
No-Git-Tag <104987021+No-Git-Tag@users.noreply.github.com>
2024-09-27 22:01:29 +0000

    Upload main script files

---
+++ installer.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#check if user runs XFCE
+if ! [ "$(pgrep -l xfce4)" ]; then
+	echo "This script is intended for XFCE!"
+	exit 1
+fi
+
+#Directories used
+INSTALL="$HOME/"
+SCRIPT="timepaper.sh"
+SCRIPT_PATH="$INSTALL/$SCRIPT"
+
+if [ ! -f "$SCRIPT" ]; then
+	echo "$SCRIPT is missing from the current directory
+	exit 1
+fi
+
+mkdir "$HOME/Pictures/Timepaper/"
+TP="$HOME/Pictures/Timepaper/"
+mkdir "$TP/Morning $TP/Day $TP/Evening $TP/Night"
+cp "$SCRIPT" "$INSTALL"
+chmod +x "$SCRIPT_PATH"
+echo "$SCRIPT has been copied to $INSTALL and made executable."
+
+AUTODIR="$HOME/.config/autostart"
+AUTOFILE="$AUTODIR/timepaper.desktop"
+
+mkdir -p "$AUTODIR"
+
+echo "[Desktop Entry]
+Type=Application
+Name= Timepaper (XFCE)
+Exec=bash -c $SCRIPT_PATH
+NoDisplay=false
+Comment=Changes wallpaper based on time of day" > "$AUTOFILE"
+
+echo "Autostart entry created at "$AUTOFILE"
+
+echo "Installation complete. Script should run at login"
---
+++ timepaper.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Define directories for each time of day
+MORNING="$HOME/Pictures/Timepaper/Morning/"
+DAY="$HOME/Pictures/Timepaper/Day/"
+EVENING="$HOME/Pictures/Timepaper/Evening/"
+NIGHT="$HOME/Pictures/Timepaper/Night/"
+
+# Function to change the wallpaper based on time of day
+change_wallpaper() {
+    # Fetch time
+    HOUR=$(date +'%H')
+
+    # Determine directory to use based on time
+    if [ $HOUR -ge 6 ] && [ $HOUR -lt 10 ] || [ $HOUR -ge 17 ] && [ $HOUR -lt 20 ]; then
+    	# is Morning and Evening
+        DIR1="$MORNING"
+        DIR2="$EVENING"
+        DIR3="$NIGHT"
+    elif [ $HOUR -ge 10 ] && [ $HOUR -lt 17 ]; then
+    	# is Day
+        DIR1="$DAY"
+        DIR2="$EVENING"
+        DIR3="$MORNING"
+    else
+    	# is Night
+        DIR1="$NIGHT"
+        DIR2="$MORNING"
+        DIR3=""
+    fi
+
+    # Set the wallpaper for workspaces 3-6
+    for WORKSPACE in {2..6}; do
+    
+    	WALLPAPERS=$(find "$DIR1" "$DIR2" "$DIR3" -type f | shuf -n 1)
+    	echo $WALLPAPERS
+    	
+        xfconf-query -c xfce4-desktop -p "/backdrop/screen0/monitoreDP-1/workspace${WORKSPACE}/last-image" -s "$WALLPAPERS"
+    done
+}
+
+# Infinite loop to change wallpaper by selected time (seconds/60)
+while true; do
+    change_wallpaper
+    sleep 900
+done
